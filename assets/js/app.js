13c2d28b241883521f8da7500eb3e9fa8e066da7
avinashdahiya <addahiya2002@gmail.com>
2025-02-25 13:54:41 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+const navbarbtn = () => {
+    document.getElementById("nav-name").classList.toggle("show-navbar")
+    document.getElementById("rotats").classList.toggle("rotate40")
+    document.getElementById("transparent").classList.toggle("bg-transparent")
+    document.getElementById("rotats2").classList.toggle("rotate-45")
+    document.body.classList.toggle("overflow-hidden")
+}
+$('.slider-content').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+    prevArrow: false,
+    nextArrow: false,
+});
